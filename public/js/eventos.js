ac9b71e17b4afa2ebf70d9a86e0230f4e5a2cc24
Natalia Herrera <nvherreramartinez@gmail.com>
2025-12-03 21:28:12 -0300

    actualización cronograma eventos 2026

--- public/js/eventos.js
+++ public/js/eventos.js
@@ -1,10 +1,9 @@
 export const events = [
-
     {
         clase: "jumping",
         link: "https://wa.me/5491112345678?text=Hola,%20quiero%20más%20info%20del%20evento",
-        mes: "NOV",
-        dia: "7",
+        mes: "ENE",
+        dia: "30",
         fecha: "VIER",
         categoriaIcon: "https://i.postimg.cc/Y0yZNJkS/icono-kangoo.png",
         hora: "19:30",
@@ -16,14 +15,27 @@ export const events = [
     {
         clase: "jumping",
         link: "https://wa.me/5491112345678?text=Hola,%20quiero%20más%20info%20del%20evento",
-        mes: "NOV",
-        dia: "8",
+        mes: "ENE",
+        dia: "31",
         fecha: "SAB",
         categoriaIcon: "https://i.postimg.cc/Y0yZNJkS/icono-kangoo.png",
-        hora: "16:30",
+        hora: "10:00",
+        titulo: "Domadoras",
+        gimnasio: "Energym",
+        barrio: "Villa Allende",
+        provincia: "Córdoba"
+    },
+    {
+        clase: "jumping",
+        link: "https://wa.me/5491112345678?text=Hola,%20quiero%20más%20info%20del%20evento",
+        mes: "ENE",
+        dia: "31",
+        fecha: "SAB",
+        categoriaIcon: "https://i.postimg.cc/Y0yZNJkS/icono-kangoo.png",
+        hora: "17:00",
         titulo: "Jumping",
-        gimnasio: "Actitud Centro",
-        barrio: "Colonia Caroya",
+        gimnasio: "Cupo Libre",
+        barrio: "Si queres sumar tu gym, academia o grupo de amig@s, contactanos!",
         provincia: "Córdoba"
     }
 ];
